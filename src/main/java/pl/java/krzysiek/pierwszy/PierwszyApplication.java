e874bc1efb0b2a323ce4b7b52515a994111192c7
KrzysztofPlusko <47918367+KrzysztofPlusko@users.noreply.github.com>
2020-12-13 18:21:37 +0100

    Initial commit

---
+++ src/main/java/pl/java/krzysiek/pierwszy/PierwszyApplication.java
@@ -0,0 +1,13 @@
+package pl.java.krzysiek.pierwszy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PierwszyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PierwszyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/java/krzysiek/pierwszy/PierwszyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.java.krzysiek.pierwszy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PierwszyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
